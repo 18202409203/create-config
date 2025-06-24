f1f975763df117362907c27dce6de9a7c8d3f1bb
zhu peng ji <pengji.zhu@svfactory.com.cn>
2025-06-24 15:23:58 +0800

    chore: example

---
+++ example/special-dist/_app.config.js
@@ -0,0 +1,8 @@
+window.APP_CONFIG = {
+  "config_title": "EpicShitSpecial"
+};
+Object.freeze(window.APP_CONFIG);
+Object.defineProperty(window, "APP_CONFIG", {
+  configurable: false,
+  writable: false,
+});
